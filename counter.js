39bdc87c9c1ef687ac2179674c65bfc94f40630f
Typhe681 <spayton681@gmail.com>
2026-03-10 16:51:48 +0200

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,31 @@
+// URL ?user=
+const params = new URLSearchParams(window.location.search);
+const sitename = params.get("user");
+
+const digitPath = "./images/digits/";
+const digitsLength = 7; 
+
+async function loadCounter() {
+  try {
+    const res = await fetch(
+      `https://weirdscifi.ratiosemper.com/neocities.php?sitename=${sitename}`
+    );
+    const data = await res.json();
+    const views = data.info.views;
+
+    const digits = views.toString().padStart(digitsLength,"0").split("");
+    const counter = document.getElementById("counter");
+    counter.innerHTML = "";
+
+    digits.forEach(d => {
+      const img = document.createElement("img");
+      img.src = digitPath + d + ".gif";
+      counter.appendChild(img);
+    });
+
+  } catch(e) {
+    console.error("Visitor counter error:", e);
+  }
+}
+
+loadCounter();
